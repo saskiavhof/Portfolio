4b5cf297d4aab700064fff527dc330b45650e8c9
Saskia Van't Hof <121727706+saskiavhof@users.noreply.github.com>
2023-12-09 19:16:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function openResume() {
-    // Replace 'your_resume_link' with the actual link to your resume
     window.open('https://drive.google.com/file/d/1FO5z-9wgBYYrK89_coT-uPJHCfs7F_Bz/view?usp=sharing', '_blank');
   }
 
@@ -11,19 +10,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const h3Element = item.querySelector('h3');
     const imageElement = item.querySelector('.work-image');
 
-    // Add a click event listener to the h3 element
     h3Element.addEventListener('click', function () {
       navigateToProjectPage(index);
     });
 
-    // Add a click event listener to the image element
     imageElement.addEventListener('click', function () {
       navigateToProjectPage(index);
     });
   });
 
   function navigateToProjectPage(index) {
-    // Redirect to the corresponding project page based on the index
     if (index < projectPages.length) {
       window.location.href = projectPages[index];
     }
@@ -35,17 +31,14 @@ document.addEventListener("DOMContentLoaded", function () {
       const windowHeight = window.innerHeight;
 
       if (itemTop < windowHeight * 0.75) {
-        // Adjust the threshold (0.75) based on when you want the animation to trigger
         item.style.transform = "translateY(0)";
         item.style.opacity = "1";
       }
     });
   }
 
-  // Initial check on page load
   checkScroll();
 
-  // Check on scroll
   window.addEventListener("scroll", checkScroll);
 });
 
